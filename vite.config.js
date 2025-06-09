8dbdbff9b66e118226345ccc7f3d4db4b0221fd5
Miked0331 <mikedragun@gmail.com>
2025-06-09 17:57:23 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/money-tracker/', // ← This must match your GitHub repo name
   plugins: [react()],
-  base: '/money-tracker/', // for GitHub Pages
 });

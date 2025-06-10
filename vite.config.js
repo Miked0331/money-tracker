9fb595bfc4a99b49660d9224773b45e7262f9086
miked0331 <mikedragun@gmail.com>
2025-06-10 18:37:14 -0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/money-tracker/',  // matches your repo name
-  plugins: [react()],
-});
